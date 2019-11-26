46ebe34d2c87c4543816cc9acc6e2fa2931f1b10
Dmitry <battlebot98@gmail.com>
2019-11-27 00:20:43 +0300

    Create project

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+
+    public float yMinPosition = -300f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < yMinPosition)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TowerCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCreator : MonoBehaviour
+{
+    private Vector3 _currentPosition = new Vector3();
+
+    public int numberOfPinsOnTheBottomLayer = 4;
+    public GameObject pin;
+    public GameObject parent;
+
+    private float _pinSpacingY;
+    private float _pinSpacingZ;
+
+    void Start()
+    {
+        if (!parent.activeSelf) return;
+
+        var scales = pin.transform.localScale;
+        _pinSpacingY = scales.y * 2;
+        _pinSpacingZ = scales.z + scales.z / 5;
+        for (var i = 0; i < numberOfPinsOnTheBottomLayer; i++)
+        {
+            _currentPosition = new Vector3(0,
+                scales.y + _pinSpacingY * i,
+                0 - (_pinSpacingZ * (numberOfPinsOnTheBottomLayer - i)) / 2);
+            
+            for (var j = 0; j < numberOfPinsOnTheBottomLayer - i; j++)
+            {
+                pin.transform.position = _currentPosition;
+                Instantiate(pin, parent.transform);
+                _currentPosition += new Vector3(0, 0, _pinSpacingZ);
+            }
+        }
+    }
+}
